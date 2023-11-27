244d4b97317e8337b986a750647697c558e47667
hgsgtk <kazuki@autify.com>
2023-11-27 09:50:59 +0900

    app: instrument ping request counter

--- tutorial/server/server.go
+++ tutorial/server/server.go
@@ -4,14 +4,25 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+var pingCounter = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "ping_request_count",
+		Help: "Number of request handled by Ping Handler",
+	},
+)
+
 func ping(w http.ResponseWriter, req *http.Request) {
+	pingCounter.Inc()
 	fmt.Fprintf(w, "pong")
 }
 
 func main() {
+	prometheus.MustRegister(pingCounter)
+
 	http.HandleFunc("/ping", ping)
 	http.Handle("/metrics", promhttp.Handler())
 
