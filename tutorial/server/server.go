39aecc4c82e4e5ec9c64d3116d8f2292baec415f
hgsgtk <kazuki@autify.com>
2023-11-28 13:02:39 +0900

    app: add a custom metric

--- tutorial/server/server.go
+++ tutorial/server/server.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
@@ -15,6 +17,20 @@ var pingCounter = prometheus.NewCounter(
 	},
 )
 
+var opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+	Name: "myapp_processed_ops_total",
+	Help: "The total number of processed events",
+})
+
+func recordMetrics() {
+	go func() {
+		for {
+			opsProcessed.Inc()
+			time.Sleep(2 * time.Second)
+		}
+	}()
+}
+
 func ping(w http.ResponseWriter, req *http.Request) {
 	pingCounter.Inc()
 	fmt.Fprintf(w, "pong")
@@ -22,6 +38,8 @@ func ping(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	prometheus.MustRegister(pingCounter)
+
+	recordMetrics()
 
 	http.HandleFunc("/ping", ping)
 	http.Handle("/metrics", promhttp.Handler())
